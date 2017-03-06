899e74acb9e515e2e6ba025d6ac76a2dfa39969c
Rudy Furtado <Rudy Furtado@DESKTOP-S7GE8TT>
2017-03-05 20:30:48 -0600

    Initial commit.   Setting things up so we can have some fun.

---
+++ SpellCheckApp/src/SpellCheckApp/Controllers/WordController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpellCheckApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellCheckApp.Controllers
+{
+    public class WordController : Controller
+    {
+        [HttpGet()]
+        public IActionResult Get(string word)
+        {
+
+            var response = new Word();
+            return Ok(response);
+        }
+    }
+}
