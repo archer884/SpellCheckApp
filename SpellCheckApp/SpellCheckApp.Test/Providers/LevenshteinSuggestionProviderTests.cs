1f9cc4a3749b5960d8f6a8ecee5c2f73122bbd0a
J/A <archer884@gmail.com>
2017-03-17 16:23:48 -0500

    Implement word list dictionary
    This dictionary service works based on a list of words that does or does
    not contain a given word. The concept here is, of course, that you will
    ask if the dictionary has a word and then ask if the dictionary has any
    suggestions (but probably only if it came back as 'incorrect.'). Maybe
    this isn't all that efficient, but, still. This is what we got at the
    moment.
    
    Also, the suggestions I've implemented so far are based only on
    Levenshtein distance, which is an absolutely awful way to determine what
    would be a better spelling of your word.

---
+++ SpellCheckApp/SpellCheckApp.Test/Providers/LevenshteinSuggestionProviderTests.cs
@@ -0,0 +1,45 @@
+﻿using SpellCheckApp.Services;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace SpellCheckApp.Test.Providers
+{
+    using SystemUnderTest = LevenshteinSuggestionProvider;
+
+    public class LevenshteinSuggestionProviderTests
+    {
+        static SystemUnderTest Sut = new SystemUnderTest();
+        static string SpellcheckedWord = "hello";
+        static List<string> WordList = new List<string>
+        {
+            "hello",
+            "finally!",
+            "go away",
+            "Emma Watson is a retard",
+            "what about she for he",
+            "I shouldn't listen to these shows while I code",
+            "It just raises my blood pressure",
+            "No one wants to see Emma Watson semi-nude anyway",
+            "Ugh",
+        };
+
+        [Fact]
+        public void All_words_are_returned()
+        {
+            Assert.Equal(WordList.Count, Sut.Suggestions(SpellcheckedWord, WordList).Count());
+        }
+
+        [Fact]
+        public void Word_is_returned_as_its_own_suggestion()
+        {
+            Assert.Contains(SpellcheckedWord, Sut.Suggestions(SpellcheckedWord, WordList));
+        }
+
+        [Fact]
+        public void Words_returned_are_limited_to_count()
+        {
+            Assert.Equal(3, Sut.Suggestions(SpellcheckedWord, WordList, 3).Count());
+        }
+    }
+}
